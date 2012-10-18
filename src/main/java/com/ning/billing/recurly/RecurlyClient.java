49afe0ddd92093241f00aba33a8c4b58dc5556e3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2012-10-18 08:18:55 -0700

    client: fix Jackson deprecation warnings
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- src/main/java/com/ning/billing/recurly/RecurlyClient.java
+++ src/main/java/com/ning/billing/recurly/RecurlyClient.java
@@ -46,7 +46,9 @@ import com.ning.http.client.Response;
 
 import com.fasterxml.jackson.databind.AnnotationIntrospector;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
@@ -105,8 +107,8 @@ public class RecurlyClient {
         this.baseUrl = String.format("https://%s:%d/%s", host, port, version);
 
         final AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
-        final AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
-        final AnnotationIntrospector pair = new AnnotationIntrospector.Pair(primary, secondary);
+        final AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+        final AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
         xmlMapper.setAnnotationIntrospector(pair);
         xmlMapper.registerModule(new JodaModule());
         xmlMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
