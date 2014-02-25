294d7232a8e5b0ccbbd6248bedbb9232d430b535
Anthony Barone <anthony.barone@dealer.com>
2014-02-25 18:45:04 -0500

    Moved signature generation methods to static methods in new class, fixed tests

--- src/main/java/com/ning/billing/recurly/RecurlyClient.java
+++ src/main/java/com/ning/billing/recurly/RecurlyClient.java
@@ -22,9 +22,6 @@ import java.math.BigDecimal;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.UUID;
 
 import javax.annotation.Nullable;
 import javax.xml.bind.DatatypeConverter;
@@ -61,11 +58,6 @@ import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Response;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.google.common.base.Joiner;
-import com.google.common.io.BaseEncoding;
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 public class RecurlyClient {
 
@@ -81,12 +73,6 @@ public class RecurlyClient {
     private static final String LINK_HEADER_NAME = "Link";
 
     public static final String FETCH_RESOURCE = "/recurly_js/result";
-
-    // Specific to Recurly.js signature generation method
-    public static final String PARAMETER_FORMAT = "%s=%s";
-    public static final String PARAMETER_SEPARATOR = "&";
-    public static final String NONCE_PARAMETER = "nonce";
-    public static final String TIMESTAMP_PARAMETER = "timestamp";
 
     /**
      * Checks a system property to see if debugging output is
@@ -694,79 +680,6 @@ public class RecurlyClient {
 
     private <T> T fetch(final String recurlyToken, final Class<T> clazz) {
         return doGET(FETCH_RESOURCE + "/" + recurlyToken, clazz);
-    }
-
-    /**
-     * Get Recurly.js Signature
-     * See spec here: http://docs.recurly.com/api/recurlyjs/signatures
-     * <p/>
-     * Returns a signature key for use with recurly.js BuildSubscriptionForm.
-     *
-     * @param privateJsKey recurly.js private key
-     * @return signature string on success, null otherwise
-     */
-    public String getRecurlySignature(String privateJsKey) {
-        return getRecurlySignature(privateJsKey, new ArrayList<String>());
-    }
-
-    /**
-     * Get Recurly.js Signature
-     * See spec here: http://docs.recurly.com/api/recurlyjs/signatures
-     * <p/>
-     * Returns a signature key for use with recurly.js BuildSubscriptionForm.
-     *
-     * @param privateJsKey recurly.js private key
-     * @param extraParams extra parameters to include in the signature
-     * @return signature string on success, null otherwise
-     */
-    public String getRecurlySignature(String privateJsKey, List<String> extraParams) {
-        final long unixTime = System.currentTimeMillis() / 1000L;
-        final String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-        return getRecurlySignature(privateJsKey, unixTime, uuid, extraParams);
-    }
-
-    /**
-     * Get Recurly.js Signature with extra parameter strings in the format "[param]=[value]"
-     * See spec here: http://docs.recurly.com/api/recurlyjs/signatures
-     * <p/>
-     * Returns a signature key for use with recurly.js BuildSubscriptionForm.
-     *
-     * @param privateJsKey recurly.js private key
-     * @param unixTime Unix timestamp, i.e. elapsed seconds since Midnight, Jan 1st 1970, UTC
-     * @param nonce A randomly generated string (number used only once)
-     * @param extraParams extra parameters to include in the signature
-     * @return signature string on success, null otherwise
-     */
-    public String getRecurlySignature(String privateJsKey, Long unixTime, String nonce, List<String> extraParams) {
-        // Mandatory parameters shared by all signatures (as per spec)
-        extraParams = (extraParams == null) ? new ArrayList<String>() : extraParams;
-        extraParams.add(String.format(PARAMETER_FORMAT, TIMESTAMP_PARAMETER, unixTime));
-        extraParams.add(String.format(PARAMETER_FORMAT, NONCE_PARAMETER, nonce));
-        String protectedParams = Joiner.on(PARAMETER_SEPARATOR).join(extraParams);
-
-        return generateRecurlyHMAC(privateJsKey, protectedParams) + "|" + protectedParams;
-    }
-
-    /**
-     * HMAC-SHA1 Hash Generator - Helper method
-     * <p/>
-     * Returns a signature key for use with recurly.js BuildSubscriptionForm.
-     *
-     * @param privateJsKey recurly.js private key
-     * @param protectedParams protected parameter string in the format: &lt;secure_hash&gt;|&lt;protected_string&gt;
-     * @return subscription object on success, null otherwise
-     */
-    private String generateRecurlyHMAC(String privateJsKey, String protectedParams) {
-        try {
-            SecretKey sk = new SecretKeySpec(privateJsKey.getBytes(), "HmacSHA1");
-            Mac mac = Mac.getInstance("HmacSHA1");
-            mac.init(sk);
-            byte[] result = mac.doFinal(protectedParams.getBytes("UTF-8"));
-            return BaseEncoding.base16().encode(result).toLowerCase();
-        } catch (Exception e) {
-            log.error("Error while trying to generate Recurly HMAC signature", e);
-            return null;
-        }
     }
 
     ///////////////////////////////////////////////////////////////////////////
---
+++ src/test/java/com/ning/billing/recurly/TestRecurlyJs.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2010-2013 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.billing.recurly;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+public class TestRecurlyJs {
+
+    @Test(groups = "fast")
+    public void testGetRecurlySignature() throws Exception {
+        final String jsPrivateKey = "0123456789abcdef0123456789abcdef";
+        final Long mockUnixTime = 1329942896L;
+        final String mockNonce = "unique";
+        final String expected = "0da7006b2093fd3d2d24a1f1f414bd9a5810a689|timestamp=1329942896&nonce=unique";
+
+        String actual = RecurlyJs.getRecurlySignature(jsPrivateKey, mockUnixTime, mockNonce, null);
+        System.out.println(actual);
+        System.out.println(expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(groups = "fast")
+    public void testGetRecurlySignatureCustomParams() throws Exception {
+        final String PARAMETER_FORMAT = "%s=%s";
+        final String SUBSCRIPTION_PARAMETER = "subscription%5Bplan_code%5D";
+        final String ACCOUNT_CODE_PARAMETER = "account%5Baccount_code%5D";
+
+        final String jsPrivateKey = "0123456789abcdef0123456789abcdef";
+        final Long mockUnixTime = 1329942896L;
+        final String mockNonce = "unique";
+        final String expected = "aa2743b6e686bf50940881733f2da37b551804f5|subscription%5Bplan_code%5D=testsub&account%5Baccount_code%5D=johndoe123&timestamp=1329942896&nonce=unique";
+
+        ArrayList<String> customParams = new ArrayList<String>();
+        customParams.add(String.format(PARAMETER_FORMAT, SUBSCRIPTION_PARAMETER, "testsub"));
+        customParams.add(String.format(PARAMETER_FORMAT, ACCOUNT_CODE_PARAMETER, "johndoe123"));
+
+        String actual = RecurlyJs.getRecurlySignature(jsPrivateKey, mockUnixTime, mockNonce, customParams);
+        System.out.println(actual);
+        System.out.println(expected);
+        Assert.assertEquals(actual, expected);
+    }
+}
