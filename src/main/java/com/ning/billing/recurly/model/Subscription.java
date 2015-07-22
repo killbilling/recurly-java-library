b6107c44c0267dc2c2b11d3e21587a0ad28fb9c3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-07-22 09:32:45 -0700

    copyright: update copyright headers
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- src/main/java/com/ning/billing/recurly/model/Subscription.java
+++ src/main/java/com/ning/billing/recurly/model/Subscription.java
@@ -1,10 +1,10 @@
 /*
  * Copyright 2010-2014 Ning, Inc.
- * Copyright 2015 Pierre-Alexandre Meyer
+ * Copyright 2014-2015 The Billing Project, LLC
  *
- * Pierre-Alexandre Meyer licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
