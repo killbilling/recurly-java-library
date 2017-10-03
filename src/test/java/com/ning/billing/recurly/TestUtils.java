4501779bd1779a007a382e2bed681e3f8a07c5b6
Benjamin Eckel <bhelx@simst.im>
2017-10-03 11:14:45 -0500

    ZIP must be valid for country in tests

--- src/test/java/com/ning/billing/recurly/TestUtils.java
+++ src/test/java/com/ning/billing/recurly/TestUtils.java
@@ -223,19 +223,6 @@ public class TestUtils {
         return random.nextInt(upperRange);
     }
 
-    /**
-     * Returns a random 2 character country code.
-     *
-     * @param seed The RNG seed
-     * @return The random country code
-     */
-    public static String randomCountry(final int seed) {
-        Random random = new Random(seed);
-        final String[] countries = {"US", "CA", "MX", "IT", "CH"};
-        final int position = random.nextInt(countries.length);
-        return countries[position];
-    }
-
     public static String createTestCCNumber() {
         return "4111-1111-1111-1111";
     }
@@ -305,7 +292,7 @@ public class TestUtils {
         address.setCity(randomAlphaNumericString(10, seed));
         address.setState(randomAlphaString(2, seed).toUpperCase());
         address.setZip("94110");
-        address.setCountry(randomCountry(seed));
+        address.setCountry("US");
         address.setPhone(randomNumericString(10, seed));
 
         return address;
@@ -335,7 +322,7 @@ public class TestUtils {
         address.setState(randomAlphaString(2, seed).toUpperCase());
         address.setState(randomAlphaString(2, seed).toUpperCase());
         address.setZip("94110");
-        address.setCountry(randomCountry(seed));
+        address.setCountry("US");
         address.setPhone(randomNumericString(10, seed));
         address.setNickname(randomAlphaNumericString(10, seed));
         address.setFirstName(randomAlphaNumericString(10, seed));
@@ -402,7 +389,7 @@ public class TestUtils {
         info.setCity(randomAlphaNumericString(10, seed));
         info.setState("CA");
         info.setZip("94110");
-        info.setCountry(randomCountry(seed));
+        info.setCountry("US");
         info.setPhone(randomInteger(8, seed));
         info.setVatNumber(randomNumericString(8, seed));
         info.setYear(createTestCCYear());
