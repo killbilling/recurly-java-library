0bea4503c4d4015d1eab04c8d7bf1c1604a6881c
Benjamin Eckel <bhelx@simst.im>
2018-10-24 14:51:51 -0500

    Update refund calls to support credit memos
    Deprecating current way of doing refunds for a single refund method
    which now supports all possible options.

--- src/main/java/com/ning/billing/recurly/model/InvoiceRefund.java
+++ src/main/java/com/ning/billing/recurly/model/InvoiceRefund.java
@@ -18,6 +18,7 @@
 package com.ning.billing.recurly.model;
 
 import com.google.common.base.Objects;
+import org.joda.time.DateTime;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
@@ -35,6 +36,21 @@ public class InvoiceRefund extends RecurlyObject {
     @XmlElementWrapper(name = "line_items")
     @XmlElement(name = "adjustment")
     private List<AdjustmentRefund> lineItems;
+
+    @XmlElement(name = "external_refund")
+    private Boolean externalRefund;
+
+    @XmlElement(name = "credit_customer_notes")
+    private String creditCustomerNotes;
+
+    @XmlElement(name = "payment_method")
+    private String paymentMethod;
+
+    @XmlElement(name = "description")
+    private String description;
+
+    @XmlElement(name = "refunded_at")
+    private DateTime refundedAt;
 
     public void setRefundMethod(final RefundMethod refundMethod) {
         this.refundMethod = refundMethod;
@@ -60,6 +76,46 @@ public class InvoiceRefund extends RecurlyObject {
         return this.lineItems;
     }
 
+    public String getCreditCustomerNotes() {
+        return creditCustomerNotes;
+    }
+
+    public void setCreditCustomerNotes(final Object creditCustomerNotes) {
+        this.creditCustomerNotes = stringOrNull(creditCustomerNotes);
+    }
+
+    public void setExternalRefund(final Object externalRefund) {
+        this.externalRefund = booleanOrNull(externalRefund);
+    }
+
+    public Boolean getExternalRefund() {
+        return this.externalRefund;
+    }
+
+    public void setPaymentMethod(final Object paymentMethod) {
+        this.paymentMethod = stringOrNull(paymentMethod);
+    }
+
+    public String getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
+    public void setDescription(final Object description) {
+        this.description = stringOrNull(description);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setRefundedAt(final Object refundedAt) {
+        this.refundedAt = dateTimeOrNull(refundedAt);
+    }
+
+    public DateTime getRefundedAt() {
+        return this.refundedAt;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hashCode(refundMethod, amountInCents);
@@ -75,7 +131,22 @@ public class InvoiceRefund extends RecurlyObject {
         if (amountInCents != null ? !amountInCents.equals(refund.amountInCents) : refund.amountInCents != null) {
             return false;
         }
+        if (externalRefund != null ? !externalRefund.equals(refund.externalRefund) : refund.externalRefund != null) {
+            return false;
+        }
         if (refundMethod != null ? !refundMethod.equals(refund.refundMethod) : refund.refundMethod != null) {
+            return false;
+        }
+        if (creditCustomerNotes != null ? !creditCustomerNotes.equals(refund.creditCustomerNotes) : refund.creditCustomerNotes != null) {
+            return false;
+        }
+        if (description != null ? !description.equals(refund.description) : refund.description != null) {
+            return false;
+        }
+        if (paymentMethod != null ? !paymentMethod.equals(refund.paymentMethod) : refund.paymentMethod != null) {
+            return false;
+        }
+        if (refundedAt != null ? refundedAt.compareTo(refund.refundedAt) != 0: refund.refundedAt != null) {
             return false;
         }
         return true;
@@ -86,6 +157,11 @@ public class InvoiceRefund extends RecurlyObject {
         final StringBuilder sb = new StringBuilder("InvoiceRefund{");
         sb.append("amountInCents=").append(amountInCents);
         sb.append(", refundMethod='").append(refundMethod).append('\'');
+        sb.append(", externalRefund='").append(externalRefund).append('\'');
+        sb.append(", creditCustomerNotes='").append(creditCustomerNotes).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", paymentMethod='").append(paymentMethod).append('\'');
+        sb.append(", refundedAt='").append(refundedAt).append('\'');
         sb.append('}');
         return sb.toString();
     }
