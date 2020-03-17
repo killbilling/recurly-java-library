9cab8bfba12ddddcfc9d1d19673ddcf8592daee9
Joanna Sese <jmgsese@gmail.com>
2020-03-16 20:43:51 -0500

    Revert "Bump v2.26"
    This reverts commit 835d594054a2c7cf72cd56df606d147a1e15f186.

--- src/main/java/com/ning/billing/recurly/RecurlyClient.java
+++ src/main/java/com/ning/billing/recurly/RecurlyClient.java
@@ -113,7 +113,7 @@ public class RecurlyClient {
     private static final Logger log = LoggerFactory.getLogger(RecurlyClient.class);
 
     public static final String RECURLY_DEBUG_KEY = "recurly.debug";
-    public static final String RECURLY_API_VERSION = "2.26";
+    public static final String RECURLY_API_VERSION = "2.25";
 
     private static final String X_RATELIMIT_REMAINING_HEADER_NAME = "X-RateLimit-Remaining";
     private static final String X_RECORDS_HEADER_NAME = "X-Records";
